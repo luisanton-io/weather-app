2948ad5b5c9857aae67fe662b8e311a016370457
Luis Antonio Canettoli <hello@luisanton.io>
2021-02-20 15:58:16 +0100

    Update index.ts

--- src/API/index.ts
+++ src/API/index.ts
@@ -2,7 +2,7 @@ import { SearchResponse, WeatherResponse } from "../models/interfaces"
 import { UserCity } from "../models/classes"
 
 const citiesEndpoint = "https://openweathermap.org/data/2.5/find?type=like&sort=population&cnt=30&appid=439d4b804bc8187953eb36d2a8c26a02&_=1613412071764"
-const weatherEndpoint = "https://api.openweathermap.org/data/2.5/onecall?appid=44397f5076949434f62fedc9ff68bfaa&exclude=minutely&units=metric"
+const weatherEndpoint = `https://api.openweathermap.org/data/2.5/onecall?appid=${process.env.REACT_APP_API_KEY}&exclude=minutely&units=metric`
 
 export const API = {
     getCities: async (query: string) => {
